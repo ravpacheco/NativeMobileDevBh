d65c6e92c016c15e24d6bc84b8df52e7d51f8d4f
Rafael Pacheco <ravpacheco@gmail.com>
2015-04-18 13:25:35 -0300

    Create user activity

--- NativeMobileDevBh/app/src/main/java/com/meetup/nativemobiledevbh/UserActivity.java
+++ NativeMobileDevBh/app/src/main/java/com/meetup/nativemobiledevbh/UserActivity.java
@@ -4,12 +4,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
 
 
 @EActivity
 public class UserActivity extends ActionBarActivity {
 
+    @ViewById(R.id.name)
+    TextView name;
+
+    @ViewById(R.id.email)
+    TextView email;
+
+    @ViewById(R.id.login)
+    TextView login;
+
+
+    @Extra
+    User findedUser;
+
+    @AfterViews
+    void init(){
+
+        if(findedUser != null) {
+
+            name.setText(findedUser.getName());
+            email.setText(findedUser.getEmail());
+            login.setText(findedUser.getLogin());
+        }
+    }
 
 }
