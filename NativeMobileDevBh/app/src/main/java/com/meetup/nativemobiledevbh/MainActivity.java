ac14f95933430859a0f0a6e30224f9a0c041a8e2
Rafael Pacheco <ravpacheco@gmail.com>
2015-04-18 12:22:16 -0300

    Init repository

---
+++ NativeMobileDevBh/app/src/main/java/com/meetup/nativemobiledevbh/MainActivity.java
@@ -0,0 +1,14 @@
+package com.meetup.nativemobiledevbh;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity
+public class MainActivity extends ActionBarActivity {
+
+
+}
