8ba216e295782ea37c39e767a403cb90a5953572
Rafael Pacheco <ravpacheco@gmail.com>
2015-04-18 12:49:49 -0300

    Include client and userActivity

--- NativeMobileDevBh/app/src/main/java/com/meetup/nativemobiledevbh/client/GitHubClient.java
+++ NativeMobileDevBh/app/src/main/java/com/meetup/nativemobiledevbh/client/GitHubClient.java
@@ -1,12 +1,16 @@
 package com.meetup.nativemobiledevbh.client;
 
 import org.androidannotations.annotations.rest.Rest;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Rest(rootUrl = "https://api.github.com",
         converters = {MappingJackson2HttpMessageConverter.class},
         requestFactory = DefaultRequestFactory.class)
 public interface GitHubClient {
 
-
+        //@Get("/users/{userName}")
+        //@Accept(MediaType.APPLICATION_JSON)
+        //@RequiresHeader(Constants.SESSION_ID_HEADER)
+        //User getAccount(String userName);
 
 }
